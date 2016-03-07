c6ab77f642112fd49bb37ee1116166ed031306ce
Ben Klein <bklein@pivotal.io>
2016-03-07 13:20:58 -0600

    Increase scripts' wait before binding commands run
    Thanks to @jkonicki!

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,6 +1,6 @@
 cf create-service p-service-registry standard service-registry
 cf create-service p-circuit-breaker-dashboard standard circuit-breaker-dashboard
-sleep 60
+sleep 120
 pushd company && cf push -p target/company-0.0.1-SNAPSHOT.jar
 popd; sleep 30
 pushd agency && cf push -p target/agency-0.0.1-SNAPSHOT.jar
