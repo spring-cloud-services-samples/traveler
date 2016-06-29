2e9e4a8bfde76b229a0129e895c56cca73ffb4fb
Ben Klein <bklein@pivotal.io>
2016-06-29 14:28:00 -0500

    Update Bash scripts for Spring Cloud Services 1.1

--- scripts/deploy_gradle.sh
+++ scripts/deploy_gradle.sh
@@ -1,8 +1,20 @@
-cf create-service p-service-registry standard service-registry
-cf create-service p-circuit-breaker-dashboard standard circuit-breaker-dashboard
-sleep 120
+echo -n "Creating services..."
+{
+  cf create-service p-service-registry standard service-registry
+  cf create-service p-circuit-breaker-dashboard standard circuit-breaker-dashboard
+} &> /dev/null
+until [ `cf service circuit-breaker-dashboard | grep -c "succeeded"` -eq 1  ]
+do
+  echo -n "."
+done
+until [ `cf service service-registry | grep -c "succeeded"` -eq 1  ]
+do
+  echo -n "."
+done
+echo
+echo "Services created. Pushing applications."
 pushd company && cf push -p build/libs/company-0.0.1-SNAPSHOT.jar
-popd; sleep 30
+popd
 pushd agency && cf push -p build/libs/agency-0.0.1-SNAPSHOT.jar
 popd
 echo "" && echo "Done!" && echo ""
