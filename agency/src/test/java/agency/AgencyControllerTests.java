a19518beda6226235b434eec2c2b0d09e38ebd2f
Roy Clarkson <roy@clarkson.org>
2018-06-05 22:42:24 -0400

    Refactor and add tests

---
+++ agency/src/test/java/agency/AgencyControllerTests.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017-Present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package agency;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = AgencyController.class, secure = false)
+public class AgencyControllerTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private TravelAgent travelAgent;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void guide() throws Exception {
+		given(this.travelAgent.getGuide())
+				.willReturn("Albert");
+		this.mvc.perform(get("/")
+				.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Your guide will be: Albert"));
+	}
+
+}
--- company/src/main/java/company/CompanyApplication.java
+++ company/src/main/java/company/CompanyApplication.java
@@ -13,31 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package company;
+
+package company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.*;
-
-@RestController
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CompanyApplication {
 
-    @RequestMapping("/available")
-    public String available() {
-      List<String> guides = Arrays.asList("Melchior", "Gaspar", "Balthazar");
-      Random rand = new Random();
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyApplication.class, args);
+	}
 
-      int randomNum = rand.nextInt(guides.size());
-      return guides.get(randomNum);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(CompanyApplication.class, args);
-    }
 }
