6abf24de2083fe567ce2a9316c3d44b479757d09
Roy Clarkson <roy@clarkson.org>
2018-06-05 22:42:53 -0400

    Add ‘traveler-‘ prefix to service names

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,13 +1,13 @@
 echo -n "Creating services..."
 {
-  cf create-service p-service-registry standard service-registry
-  cf create-service p-circuit-breaker-dashboard standard circuit-breaker-dashboard
+  cf create-service p-service-registry standard traveler-service-registry
+  cf create-service p-circuit-breaker-dashboard standard traveler-circuit-breaker-dashboard
 } &> /dev/null
-until [ `cf service circuit-breaker-dashboard | grep -c "succeeded"` -eq 1  ]
+until [ `cf service traveler-circuit-breaker-dashboard | grep -c "succeeded"` -eq 1  ]
 do
   echo -n "."
 done
-until [ `cf service service-registry | grep -c "succeeded"` -eq 1  ]
+until [ `cf service traveler-service-registry | grep -c "succeeded"` -eq 1  ]
 do
   echo -n "."
 done
