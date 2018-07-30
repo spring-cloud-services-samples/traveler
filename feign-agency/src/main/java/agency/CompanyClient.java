14a285907a647de4daf5213e6625d911337eb29b
Ben Klein <bklein@pivotal.io>
2018-07-30 12:22:02 -0500

    Remove extra whitespace

--- feign-agency/src/main/java/agency/CompanyClient.java
+++ feign-agency/src/main/java/agency/CompanyClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package agency;
+package agency;
 
 import org.springframework.stereotype.Component;
 import org.springframework.cloud.openfeign.FeignClient;
